aded9ce6fde5ec17c611ef9e6d9dcf33ed7fd5a6
Jonathan Doan <doanjonathan@seattleu.edu>
2022-01-30 16:57:51 -0800

    Added TaskExtension to shared project, TaskExtensions.cs

---
+++ Mine/Mine/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    public static class TaskExtensions
+    {
+        public static async void SafeFireAndForget(this Task task,
+            bool returnToCallingContext, Action<Exception> onExeption = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+            // if the provided action is not null, catch and pass the thrown 
+            // exception
+            catch (Exception ex) when (onExeption != null)
+            {
+                onExeption(ex);
+            }
+        }
+    }
+}
